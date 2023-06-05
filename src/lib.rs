0721f7eec7c50d0d8f3d6a649f67d4ef87778fd7
David Rett <80925809+dprdev@users.noreply.github.com>
2023-06-05 00:33:41 -0500

    Quick update before bed
    Updating new worker logic unfinished

--- src/lib.rs
+++ src/lib.rs
@@ -70,14 +70,18 @@ pub struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+        let message = receiver.lock().unwrap().recv();
 
-            println!("Worker {id} got a job; executing.");
+        match message {
+            Ok(job) => {
+                println!("Worker {id} got a job; executing.");
 
-            job();
-        });
-
+                job();
+            }
+            Err(_) => {
+                println!("")
+            }
+        }
         Worker {
             id,
             thread: Some(thread),
